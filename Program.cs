e217522509df5182a85ef6b788e6de1a453130c3
SpanglMindCrow <asova417@gmail.com>
2022-04-16 17:53:01 +0500

    add HomeWork. Task 1.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Доброго времени суток!");
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+if (a < b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a); 
+}
+if (a==b)
+{
+     Console.WriteLine("Все фигня, они равны!"); 
+}
